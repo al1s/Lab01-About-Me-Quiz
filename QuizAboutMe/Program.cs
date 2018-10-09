88b8bb4473282f6a16e3da7b850c7e342187f684
alstof <alstof@gmail.com>
2018-10-09 12:58:38 -0700

    Add engine skeleton

--- QuizAboutMe/Program.cs
+++ QuizAboutMe/Program.cs
@@ -12,8 +12,39 @@ namespace QuizAboutMe
             new string[] { "What country besides the USA and Russia I\'ve been?", "String", "Mongolia, Spain, China, Italy, France, Switzerland, Netherlands"},
             new string[] { "What is the meaning of life if it\'s filled with surprises?", "Range" }
         };
+        static void QuizEngine()
+        {
+            foreach (string[] quizElm in quizContent)
+            {
+                string userInput = GetUserAnswerTo(quizElm[0]);
+                switch (quizElm[1])
+                {
+                case "Boolean":
+                    HandleBooleanAnswer(Convert.ToBoolean(quizElm[2]));
+                    break;
+                };
+            }            
+
+        }
+        static string GetUserAnswerTo(string question)
+        {
+            return string.Empty;
+        }
+        static int HandleBooleanAnswer(bool correctAnswer)
+        {
+            return default(int);
+        }
+        static int HandleStringAnswer(bool correctAnswer)
+        {
+            return default(int);
+        }
+        static int HandleRangeAnswer(bool correctAnswer)
+        {
+            return default(int);
+        }
         static void Main(string[] args)
         {
+            QuizEngine();
             Console.WriteLine("Hello World!");
         }
     }
